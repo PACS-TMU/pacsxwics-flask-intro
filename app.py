208a331e49f4eb882b191b9a03e78228acde987f
Saadia Shahid <saadia.shahid@torontomu.ca>
2024-10-22 08:12:36 -0400

    update project structure

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
