01b583f8213288611c6993dfd8058e561ee64e63
David Jiang <jiang.david23@gmail.com>
2024-11-05 15:13:31 -0500

    implemented projects section

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    #define our list of dictionaries where each dictionary represents a project (Image, Title, and Description)
+    projects = [{"image": "/static/images/project_img.jpg", 
+                 "title":"PROJECT TITLE", 
+                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros eros, imperdoet vitae mollis ut, sagittis et ante.",
+                 "github": "https://github.com/"},
+                {"image": "/static/images/project_img.jpg", 
+                 "title":"PROJECT TITLE", 
+                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros eros, imperdoet vitae mollis ut, sagittis et ante.",
+                 "github": "https://github.com/"},
+                {"image": "/static/images/project_img.jpg", 
+                 "title":"PROJECT TITLE", 
+                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros eros, imperdoet vitae mollis ut, sagittis et ante.",
+                 "github": "https://github.com/"},
+                {"image": "/static/images/project_img.jpg", 
+                 "title":"PROJECT TITLE", 
+                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros eros, imperdoet vitae mollis ut, sagittis et ante.",
+                 "github": "https://github.com/"}]
+    
+    return render_template('index.html', projects = projects)
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
